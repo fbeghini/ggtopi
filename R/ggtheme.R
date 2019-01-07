23261984a6a7b8dc1a298a83279196f0ddf32de4
Francesco Beghini <francesco.beghini@unitn.it>
2019-01-07 13:41:54 +0100

    added panel

--- R/ggtheme.R
+++ R/ggtheme.R
@@ -1,7 +1,14 @@
+if(!requireNamespace("showtext", quietly = TRUE)){
+  install.packages('showtext', dependencies = TRUE)
+}
+library(showtext)
+font_add_google("Roboto", "Roboto")
+font_add_google("Montserrat", "Montserrat")
+
 theme_cm <- function(){
   theme_minimal() +
-  theme(strip.background = element_rect(colour = "transparent", fill = "transparent"),
-        axis.text = element_text(color = "black", size = 10),
-        strip.text.y = element_text(angle = 0, size = 10)) +
-  scale_fill_brewer(name = "Timepoint", palette="Dark2")
+  theme(text = element_text(family = "Montserrat", size = 10, colour = "black"),
+        axis.text = element_text(family = "Montserrat", size = 10, colour = "black"),
+        strip.background = element_rect(colour = "transparent", fill = "transparent"),
+        strip.text = element_text(angle = -1, family = "Montserrat", size = 10, colour = "black"))
 }
