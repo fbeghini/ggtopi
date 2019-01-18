cae9351b1317bfa5623b60726e7f5116b70d0d86
Francesco Beghini <fbeghini@outlook.com>
2019-01-18 10:10:23 +0100

    edits

--- R/ggtheme.R
+++ R/ggtheme.R
@@ -1,16 +1,17 @@
-if(!requireNamespace("showtext", quietly = TRUE)){
-  install.packages('showtext', dependencies = TRUE)
-}
-library(showtext)
-font_add_google("Roboto", "Roboto")
-font_add_google("Montserrat", "Montserrat")
+# if(!requireNamespace("showtext", quietly = TRUE)){
+#   install.packages('showtext', dependencies = TRUE)
+#   install.packages("extrafont")
+# }
+# library(showtext)
+# font_add_google("Roboto", "Roboto")
+# font_add_google("Montserrat", "Montserrat")
 
 theme_cm <- function(){
   theme_minimal() +
-  theme(text = element_text(family = "Montserrat", size = 10, colour = "black"),
-        axis.text = element_text(family = "Montserrat", size = 10, colour = "black"),
+  theme(text = element_text(family = "sans-serif", size = 10, colour = "black"),
+        axis.text = element_text(family = "sans-serif", size = 10, colour = "black"),
         strip.background = element_rect(colour = "transparent", fill = "transparent"),
-        strip.text = element_text(family = "Montserrat", size = 12, colour = "black"))
+        strip.text = element_text(family = "sans-serif", size = 12, colour = "black"))
 }
 
 cm_palette <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#FF8F33','#FFE133','#6D3100')
